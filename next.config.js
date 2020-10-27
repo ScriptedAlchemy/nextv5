726f48e4a7045307fadfb9b40900d6c64ce83986
Zack Jackson <zackary.l.jackson@gmail.com>
2020-10-27 10:28:24 -0700

    next webpack initialize

--- next.config.js
+++ next.config.js
@@ -1,11 +1,34 @@
+const deps = require('./package.json').dependencies
 module.exports = {
-    // works with serverless too
-    // target: 'serverless',
-    onDemandEntries: false,
-    typescript: {
-        ignoreDevErrors: true,
-    },
-    experimental: {
-        modern: false
-    },
+  // works with serverless too
+  // target: 'serverless',
+  webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+console.log(config.plugin);
+    config.plugins.push(new webpack.container.ModuleFederationPlugin({
+      name: 'next1',
+      filename: 'static/remoteEntry.js',
+      remotes: {
+        next1: 'next1@http://localhost:3000/_next/static/remoteEntry.js'
+      },
+      shared: {
+        react:deps.react
+      }
+    }))
+    config.experiments = {
+      topLevelAwait: true
+    }
+
+    // Important: return the modified config
+    return config
+  },
+
+  typescript: {
+    ignoreDevErrors: true,
+  },
+  experimental: {
+    modern: false,
+    // topLevelAwait: true
+  },
 }
---
+++ shared-react.js
@@ -0,0 +1,15 @@
+if (global.React) {
+  module.exports = global.React;
+} else if (process.browser && window.React) {
+  console.log("should in browser react is there");
+  module.exports = window.React;
+} else if (process.browser) {
+  console.log("should provide react");
+  console.log(require.resolve("react"));
+  window.React = require.resolve("react");
+  module.exports = window.React;
+} else {
+  console.log("no condiitons met");
+  global.React = require("react");
+  module.exports = global.React;
+}
